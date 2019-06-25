7fa674379a9c8231fa3a79caa2237af853edbcce
nireo <eemil.lehtikangas1@gmail.com>
2019-06-25 21:58:35 +0300

    added password hashing

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -2,12 +2,42 @@ const express = require("express"),
     router = express.Router(),
     Expenses = require("../models/expense")
 
+// some basic middleware
+router.use(function timeLog (req, res, next) {
+    const time = new Date
+    // time of request
+    console.log("Time: ", time)
+    // what method e.g. POST, GET; PUT, DELETE
+    console.log("Request Type:", req.method)
+    // see which url the request goes to
+    console.log("Request URL:", req.originalUrl)
+    next()
+})
+
+// middleware for errors
+router.use((err, req, res, next) => {
+    console.error(err.stack)
+    // send a json error message
+    res.status(500).json({error: 'something went wrong'})
+})
+
 router.get("/", async (req, res) => {
     try {
         const expenses = await Expenses.find({})
         res.json(expenses.map(expense => expense.toJSON()))
     } catch (e) {
-        console.log(e)
+        console.error(e)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        // create a new time object
+        const time = new Date()
+        //clean up code
+        const body = req.body
+    } catch (e) {
+        next(e)
     }
 })
 
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,14 +2,28 @@
 const express = require("express")
 router = express.Router()
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
 // import user model
 const User = require("../models/user")
 
-// some middleware
+// some basic middleware
 router.use(function timeLog (req, res, next) {
-    console.log('Time: ', Date.now())
+    const time = new Date
+    // time of request
+    console.log("Time: ", time)
+    // what method e.g. POST, GET; PUT, DELETE
+    console.log("Request Type:", req.method)
+    // see which url the request goes to
+    console.log("Request URL:", req.originalUrl)
     next()
+})
+
+// middleware for errors
+router.use((err, req, res, next) => {
+    console.error(err.stack)
+    // send a json error message
+    res.status(500).json({error: 'something went wrong'})
 })
 
 // get route for getting users
@@ -21,28 +35,40 @@ router.get("/", async (req, res, next) => {
         res.json(users.map(user => user.toJSON()))
     } catch (e) {
         // if anything goes wrong
-        console.log(e)
+        console.error(e)
         next()
     }
 })
 
 // post route for adding users
-router.post("/", async (req, res) => {
-    // create a new time object
-    const time = new Date()
-    const body = req.body
-    const testUser = new User({
-        name: body.name,
-        username: body.username,
-        passwordHash: body.passwordHash,
-        monthlySalary: body.monthlySalary,
-        registerationDate: `${time}`,
-        expenses: []
-    })
-    // save user 
-    const savedUser = await testUser.save()
-    //return saved user data in JSON
-    res.json(savedUser.toJSON())
+router.post("/", async (req, res, next) => {
+    // try to run code without errors
+    try {
+        // create a new time object
+        const time = new Date()
+        // clean up code by not having to type e.g. req.body.name
+        const body = req.body
+
+        const salt = 10
+        const passwordHash = await bcrypt.hash(body.password, salt)
+
+        const user = new User({
+            name: body.name,
+            username: body.username,
+            passwordHash,
+            monthlySalary: 0,
+            registerationDate: `${time}`,
+            expenses: []
+        })
+        // save user 
+        const savedUser = await user.save()
+        //return saved user data in JSON
+        res.json(savedUser)
+    } catch (e) {
+        // if a error occurs
+        next(e)
+    }
+
 })
 
 // export routes
